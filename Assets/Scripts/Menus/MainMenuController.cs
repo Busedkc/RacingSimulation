8c866648966086f198032c8ebd4a2cccac2caf92
Sude Sevim <sudesevim0@gmail.com>
2024-05-13 16:54:16 +0300

    Main Menu

--- Assets/Scripts/Menus/MainMenuController.cs
+++ Assets/Scripts/Menus/MainMenuController.cs
@@ -1,18 +1,63 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
 
 public class MainMenuController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Button raceButton = null;
+    [SerializeField] Button hotPursuitButton = null;
+    [SerializeField] Button garageButton = null;
+    [SerializeField] Button shopButton = null;
+    [SerializeField] List<CarController> carList = null;
+
+    private void Awake()
     {
-        
+        foreach(CarController car in carList)
+        {
+            car.DisplayCar(false);
+        }
+        CarController activeCar = carList[GameManager.GetCurrentActiveCarIndex()];
+        activeCar.SetColorAndRims();
+        activeCar.DisplayCar(true,true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnEnable()
     {
-        
+        raceButton.onClick.AddListener(HandleRaceButtonClicked);
+        hotPursuitButton.onClick.AddListener(HandleHotPursuitButtonClicked);
+        garageButton.onClick.AddListener(HandleGarageButtonClicked);
+        shopButton.onClick.AddListener(HandleShopButtonClicked);
+    }
+
+    private void OnDisable()
+    {
+        raceButton.onClick.RemoveListener(HandleRaceButtonClicked);
+        hotPursuitButton.onClick.RemoveListener(HandleHotPursuitButtonClicked);
+        garageButton.onClick.RemoveListener(HandleGarageButtonClicked);
+        shopButton.onClick.RemoveListener(HandleShopButtonClicked);
+    }
+    
+    void HandleRaceButtonClicked()
+    {
+        Events.OkButtonClicked?.Invoke();
+        NavigationManager.LoadScene(Scenes.SELECT_A_TRACK);
+    }
+
+    void HandleHotPursuitButtonClicked()
+    {
+        Events.OkButtonClicked?.Invoke();
+        NavigationManager.LoadScene(Scenes.CITY);
+    }
+
+    void HandleGarageButtonClicked()
+    {
+        Events.OkButtonClicked?.Invoke();
+        NavigationManager.LoadScene(Scenes.GARAGE);
+    }
+
+    void HandleShopButtonClicked()
+    {
+        Events.OkButtonClicked?.Invoke();
+        NavigationManager.LoadScene(Scenes.SHOP);
     }
 }
