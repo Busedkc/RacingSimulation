da97dee090ef175fce8ba3817c3776ab39c24a25
Sude Sevim <sudesevim0@gmail.com>
2024-05-20 22:23:43 +0300

    Speedometer, Leaderboard Panel and Debug

--- Assets/Scripts/UI/Speedometer.cs
+++ Assets/Scripts/UI/Speedometer.cs
@@ -1,18 +1,35 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Speedometer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<CarController> cars = null;
+    [SerializeField] GameObject speedometerNeedle = null;
+
+    const float minRotation = 135.0f;
+    const float maxRotation = -135.0f;
+    const float maxSpeed = 50.0f;
+    float rotation;
+
+    CarController car;
+
+    private void Awake()
     {
-        
+        car = cars[GameManager.GetCurrentActiveCarIndex()];
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        UpdateSpeedometer();
+    }
+
+    void UpdateSpeedometer()
+    {
+        float currentSpeedPercentage = car.GetSpeed() / maxSpeed;
+        rotation = currentSpeedPercentage >= 0 ? Mathf.Lerp(minRotation, maxRotation, currentSpeedPercentage) :
+                                                 Mathf.Lerp(minRotation,maxRotation, -currentSpeedPercentage) ;
+        speedometerNeedle.transform.eulerAngles = new Vector3(speedometerNeedle.transform.eulerAngles.x,
+                                                              speedometerNeedle.transform.eulerAngles.y,
+                                                              rotation);
     }
 }
