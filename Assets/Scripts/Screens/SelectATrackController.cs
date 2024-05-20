67f9dd89855c986e04aa0e4f672616bdc84c6642
Busedkc <busedikici001@gmail.com>
2024-05-20 10:21:27 +0300

    SelectATrackController.cs

--- Assets/Scripts/Screens/SelectATrackController.cs
+++ Assets/Scripts/Screens/SelectATrackController.cs
@@ -1,18 +1,51 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SelectATrackController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<GameObject> tracks = null;
+    [SerializeField] Button selectTrackButton = null;
+    [SerializeField] TrackCarousel trackCarousel = null;
+
+    private void Start()
     {
-        
+        selectTrackButton.onClick.AddListener(HandleSelectTrackButtonClick);
+    }
+    private void OnDestroy()
+    {
+        selectTrackButton.onClick.RemoveListener(HandleSelectTrackButtonClick);
+    }
+    private void Update()
+    {
+        TurnTracksToFaceCamera();
     }
 
-    // Update is called once per frame
-    void Update()
+    void TurnTracksToFaceCamera()
     {
-        
+        foreach(GameObject track in tracks)
+        {
+            track.transform.LookAt(Camera.main.transform);
+        }
+    }
+
+    void HandleSelectTrackButtonClick()
+    {
+        Dictionary<string, string> sceneData = new Dictionary<string, string>();
+        switch (trackCarousel.GetCurrentItemIndex())
+        {
+            case 0:
+                sceneData.Add("track", "1");
+                NavigationManager.LoadScene(Scenes.TRACK1, sceneData);
+                break;
+            case 1:
+                sceneData.Add("track", "2");
+                NavigationManager.LoadScene(Scenes.TRACK2, sceneData);
+                break;
+            case 2:
+                sceneData.Add("track", "3");
+                NavigationManager.LoadScene(Scenes.TRACK3, sceneData);
+                break;
+        }
     }
 }
