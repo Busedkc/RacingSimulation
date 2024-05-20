26e82187123beb1c0916ced8e571aca021a680c6
Busedkc <busedikici001@gmail.com>
2024-05-20 10:05:55 +0300

    TrackCarousel.cs

--- Assets/Scripts/Carousels/TrackCarousel.cs
+++ Assets/Scripts/Carousels/TrackCarousel.cs
@@ -1,18 +1,81 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class TrackCarousel : MonoBehaviour
+public class TrackCarousel : MonoBehaviour, ICarousel
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] List<GameObject> tracks = null;
+    [SerializeField] Button leftButton = null;
+    [SerializeField] Button rightButton = null;
+    [SerializeField] Transform pivot = null;
+    [SerializeField] float carouselRotateSpeed;
+    int currentActiveIndex;
+    bool rotateLeft;
+    bool rotateRight;
+    Quaternion leftRotationEnd;
+    Quaternion rightRotationEnd;
+
+    private void Start()
     {
-        
+        leftButton.onClick.AddListener(ShowPreviousItem);
+        rightButton.onClick.AddListener(ShowNextItem);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDestroy()
     {
-        
+        leftButton.onClick.RemoveListener(ShowPreviousItem);
+        rightButton.onClick.RemoveListener(ShowNextItem);
+    }
+
+    public int GetCurrentItemIndex()
+    {
+        return currentActiveIndex;
+    }
+
+    public void ShowNextItem()
+    {
+        if(rotateLeft || rotateRight)
+        {
+            return;
+        }
+        Events.LeftOrRightButtonClicked?.Invoke();
+        currentActiveIndex = currentActiveIndex < tracks.Count - 1 ? currentActiveIndex + 1 : 0;
+        rightRotationEnd = pivot.transform.rotation * Quaternion.Euler(0, -120, 0);
+        rotateRight = true;
+
+    }
+
+    public void ShowPreviousItem()
+    {
+        if(rotateLeft || rotateRight)
+        {
+            return;
+        }
+        Events.LeftOrRightButtonClicked?.Invoke();
+        currentActiveIndex = currentActiveIndex > 0 ? currentActiveIndex - 1 : tracks.Count - 1;
+        leftRotationEnd = pivot.transform.rotation * Quaternion.Euler(0, 120, 0);
+        rotateLeft = true;
+    }
+
+    private void Update()
+    {
+        if(rotateLeft)
+        {
+            pivot.transform.rotation = Quaternion.Lerp(pivot.transform.rotation, leftRotationEnd,
+                                                            carouselRotateSpeed * Time.deltaTime);
+        }
+        if(rotateRight)
+        {
+            pivot.transform.rotation = Quaternion.Lerp(pivot.transform.rotation, rightRotationEnd,
+                                                            carouselRotateSpeed * Time.deltaTime);
+        }
+        if(rotateLeft && pivot.transform.rotation == leftRotationEnd)
+        {
+            rotateLeft = false;
+        }
+        if (rotateRight && pivot.transform.rotation == rightRotationEnd)
+        {
+            rotateRight = false;
+        }
     }
 }
