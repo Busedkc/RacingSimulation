00454690cfb45b89831a23b758802eddb9cbc6d7
Busedkc <busedikici001@gmail.com>
2024-05-23 16:44:51 +0300

    animated cutscene, 3D character

--- Assets/Scripts/Cutscenes/Arrested/PoliceOfficer.cs
+++ Assets/Scripts/Cutscenes/Arrested/PoliceOfficer.cs
@@ -1,18 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PoliceOfficer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform target = null;
+    [SerializeField] float speed;
+    [SerializeField] OneButtonModal modal = null;
+    [SerializeField] Animator animator = null;
+    [SerializeField] AudioSource suspectInCustodyAudio = null;
+    bool audioPlayed;
+
+    private void Awake()
     {
-        
+        modal.Init(BackToMainMenu);
+        modal.HideModal();
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        if(Vector3.Distance(transform.position, target.transform.position) < 0.001f)
+        {
+            if(!audioPlayed)
+            {
+                audioPlayed = true;
+                suspectInCustodyAudio.Play();
+            }
+            modal.ShowModal();
+            animator.enabled = false;
+
+        }
+    }
+
+    void BackToMainMenu()
+    {
+        NavigationManager.LoadScene(Scenes.MAIN_MENU);
     }
 }
